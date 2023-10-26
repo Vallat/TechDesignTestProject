890b45811c1afab81c02973cbc40e9f4543c26da
Vallat <vox.crit2013@gmail.com>
2023-10-26 07:19:46 +0300

    added three different animations to scenes

---
+++ Assets/Scripts/shared/anim_play_on_click.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anim_play_on_click : MonoBehaviour
+{
+    private Animator anim = null;
+    public string anim_name = null;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (GetComponent<Collider2D>().OverlapPoint(mousePosition))
+            {
+                anim.enabled = true;
+                anim.Update(0f);
+                anim.Play(anim_name, 0, 0);
+            }
+        }
+    }
+}
